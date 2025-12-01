e2bf96dcc5ac689c5aa943911dec593a68499c93
Quentin Hable <quentin.hable@student.ucll.be>
2025-12-01 19:00:42 +0100

    products data gen

--- physical_model.sql
+++ physical_model.sql
@@ -1,26 +1,27 @@
-
 CREATE  TABLE group_7.client ( 
-	id                   integer  NOT NULL  ,
+	id                   char(8)  NOT NULL  ,
 	address              varchar(100)  NOT NULL  ,
 	phone_number         integer  NOT NULL  ,
 	CONSTRAINT pk_client PRIMARY KEY ( id )
  );
 
 CREATE  TABLE group_7.email ( 
-	client_id            integer  NOT NULL  ,
-	email                varchar(70)    
+	client_id            char(8)  NOT NULL  ,
+	email                varchar(70)    ,
+	CONSTRAINT client_id FOREIGN KEY ( client_id ) REFERENCES group_7.client( id )   
  );
 
 CREATE INDEX idx_email ON group_7.email  ( client_id, email );
 
 CREATE  TABLE group_7.patient ( 
-	id                   integer  NOT NULL  ,
+	id                   char(8)  NOT NULL  ,
 	name                 varchar(100)  NOT NULL  ,
 	date_of_birth        date  NOT NULL  ,
 	animal_species       varchar(70)  NOT NULL  ,
 	animal_breed         varchar(70)  NOT NULL  ,
-	client_number        integer  NOT NULL  ,
-	CONSTRAINT pk_patient PRIMARY KEY ( id )
+	client_id            char(8)  NOT NULL  ,
+	CONSTRAINT pk_patient PRIMARY KEY ( id ),
+	CONSTRAINT client_id FOREIGN KEY ( client_id ) REFERENCES group_7.client( id )   
  );
 
 CREATE  TABLE group_7.practice ( 
@@ -32,7 +33,7 @@ CREATE  TABLE group_7.practice (
  );
 
 CREATE  TABLE group_7.products ( 
-	id                   integer  NOT NULL  ,
+	id                   char(8)  NOT NULL  ,
 	product_name         varchar(100)  NOT NULL  ,
 	product_price        integer  NOT NULL  ,
 	product_category     varchar(100)    ,
@@ -43,7 +44,8 @@ CREATE  TABLE group_7.products (
 CREATE  TABLE group_7.room ( 
 	id                   integer  NOT NULL  ,
 	practice_name        varchar(100)  NOT NULL  ,
-	CONSTRAINT pk_room PRIMARY KEY ( id )
+	CONSTRAINT pk_room PRIMARY KEY ( id ),
+	CONSTRAINT fk_practice FOREIGN KEY ( practice_name ) REFERENCES group_7.practice( id )   
  );
 
 CREATE  TABLE group_7.time_slot ( 
@@ -54,22 +56,25 @@ CREATE  TABLE group_7.time_slot (
  );
 
 CREATE  TABLE group_7.worker ( 
-	id                   integer  NOT NULL  ,
+	id                   char(8)  NOT NULL  ,
 	employee_name        varchar(50)  NOT NULL  ,
 	employee_address     varchar(100)  NOT NULL  ,
 	phone_number         integer  NOT NULL  ,
 	employee_roll        varchar(100)  NOT NULL  ,
 	practice_name        varchar(100)  NOT NULL  ,
-	CONSTRAINT pk_worker PRIMARY KEY ( id )
+	CONSTRAINT pk_worker PRIMARY KEY ( id ),
+	CONSTRAINT fk_practice FOREIGN KEY ( practice_name ) REFERENCES group_7.practice( id )   
  );
 
 CREATE  TABLE group_7.worker_has_time_slot ( 
-	worker_id            integer  NOT NULL  ,
-	time_slot_id         timestamp  NOT NULL  
+	worker_id            char(8)  NOT NULL  ,
+	time_slot_id         timestamp  NOT NULL  ,
+	CONSTRAINT fk_time_slot FOREIGN KEY ( time_slot_id ) REFERENCES group_7.time_slot( id )   ,
+	CONSTRAINT worker_id FOREIGN KEY ( worker_id ) REFERENCES group_7.worker( id )   
  );
 
 CREATE  TABLE group_7.appointment ( 
-	id                   integer  NOT NULL  ,
+	id                   char(10)  NOT NULL  ,
 	appointment_date     date  NOT NULL  ,
 	description          varchar(100)    ,
 	start_time           time  NOT NULL  ,
@@ -77,85 +82,61 @@ CREATE  TABLE group_7.appointment (
 	results              varchar(100)    ,
 	appointment_type     varchar  NOT NULL  ,
 	appointment_cost     smallint  NOT NULL  ,
-	follow_up_of         integer    ,
-	patient_id           integer  NOT NULL  ,
+	follow_up_of         char(10)    ,
+	patient_id           char(8)  NOT NULL  ,
 	room_number          integer  NOT NULL  ,
-	CONSTRAINT pk_appointment PRIMARY KEY ( id )
+	CONSTRAINT pk_appointment PRIMARY KEY ( id ),
+	CONSTRAINT appointment_id FOREIGN KEY ( follow_up_of ) REFERENCES group_7.appointment( id )   ,
+	CONSTRAINT patient_id FOREIGN KEY ( patient_id ) REFERENCES group_7.patient( id )   ,
+	CONSTRAINT room_id FOREIGN KEY ( room_number ) REFERENCES group_7.room( id )   
  );
 
 CREATE  TABLE group_7.client_orders_product ( 
-	product_id           integer  NOT NULL  ,
-	client_number        integer  NOT NULL  ,
+	product_id           char(8)  NOT NULL  ,
+	client_id            char(8)  NOT NULL  ,
 	order_date           date  NOT NULL  ,
-	order_amount         integer  NOT NULL  
+	order_amount         integer  NOT NULL  ,
+	CONSTRAINT client_id FOREIGN KEY ( client_id ) REFERENCES group_7.client( id )   ,
+	CONSTRAINT product_id FOREIGN KEY ( product_id ) REFERENCES group_7.products( id )   
  );
 
-CREATE INDEX idx_client_orders_product ON group_7.client_orders_product  ( product_id, client_number );
+CREATE INDEX idx_client_orders_product ON group_7.client_orders_product  ( product_id, client_id );
 
 CREATE  TABLE group_7.prescribes ( 
-	product_id           integer  NOT NULL  ,
-	appointment_id       integer  NOT NULL  ,
+	product_id           char(8)  NOT NULL  ,
+	appointment_id       char(10)  NOT NULL  ,
 	prescription_duration varchar(20)  NOT NULL  ,
-	prescription_amount  integer  NOT NULL  
+	prescription_amount  integer  NOT NULL  ,
+	CONSTRAINT product_id FOREIGN KEY ( product_id ) REFERENCES group_7.products( id )   ,
+	CONSTRAINT appointment_id FOREIGN KEY ( appointment_id ) REFERENCES group_7.appointment( id )   
  );
+
+CREATE INDEX idx_prescribes ON group_7.prescribes  ( product_id, appointment_id );
 
 CREATE  TABLE group_7.specialisation ( 
 	id                   varchar(100)  NOT NULL  ,
-	employee_id          integer  NOT NULL  ,
-	CONSTRAINT pk_specialisation PRIMARY KEY ( id )
+	worker_id            char(8)  NOT NULL  ,
+	CONSTRAINT pk_specialisation PRIMARY KEY ( id ),
+	CONSTRAINT worker_id FOREIGN KEY ( worker_id ) REFERENCES group_7.worker( id )   
  );
 
 CREATE  TABLE group_7.vet_has_appointment ( 
-	appointment_id       integer  NOT NULL  ,
-	employee_id          integer  NOT NULL  
+	appointment_id       char(10)  NOT NULL  ,
+	worker_id            char(8)  NOT NULL  ,
+	CONSTRAINT appointment_id FOREIGN KEY ( appointment_id ) REFERENCES group_7.appointment( id )   ,
+	CONSTRAINT worker_id FOREIGN KEY ( worker_id ) REFERENCES group_7.worker( id )   
  );
 
-CREATE INDEX idx_vet_has_appointment ON group_7.vet_has_appointment  ( appointment_id, employee_id );
+CREATE INDEX idx_vet_has_appointment ON group_7.vet_has_appointment  ( appointment_id, worker_id );
 
 CREATE  TABLE group_7.animal_carer_has_appointment ( 
-	appointment_id       integer  NOT NULL  ,
-	employee_id          integer  NOT NULL  
+	appointment_id       char(10)  NOT NULL  ,
+	worker_id            char(8)  NOT NULL  ,
+	CONSTRAINT worker_id FOREIGN KEY ( worker_id ) REFERENCES group_7.worker( id )   ,
+	CONSTRAINT appointment_id FOREIGN KEY ( appointment_id ) REFERENCES group_7.appointment( id )   
  );
 
-CREATE INDEX idx_animal_carer_has_appointment ON group_7.animal_carer_has_appointment  ( appointment_id, employee_id );
-
-CREATE SCHEMA IF NOT EXISTS group_7;
-
-ALTER TABLE group_7.animal_carer_has_appointment ADD CONSTRAINT fk_appointment FOREIGN KEY ( appointment_id ) REFERENCES group_7.appointment( id );
-
-ALTER TABLE group_7.animal_carer_has_appointment ADD CONSTRAINT fk_worker FOREIGN KEY ( employee_id ) REFERENCES group_7.worker( id );
-
-ALTER TABLE group_7.appointment ADD CONSTRAINT fk_follow_up FOREIGN KEY ( follow_up_of ) REFERENCES group_7.appointment( id );
-
-ALTER TABLE group_7.appointment ADD CONSTRAINT fk_patient FOREIGN KEY ( patient_id ) REFERENCES group_7.patient( id );
-
-ALTER TABLE group_7.appointment ADD CONSTRAINT fk_room FOREIGN KEY ( room_number ) REFERENCES group_7.room( id );
-
-ALTER TABLE group_7.client_orders_product ADD CONSTRAINT product_id FOREIGN KEY ( product_id ) REFERENCES group_7.products( id );
-
-ALTER TABLE group_7.client_orders_product ADD CONSTRAINT client_number FOREIGN KEY ( client_number ) REFERENCES group_7.client( id );
-
-ALTER TABLE group_7.email ADD CONSTRAINT fk_email_client FOREIGN KEY ( client_id ) REFERENCES group_7.client( id );
-
-ALTER TABLE group_7.patient ADD CONSTRAINT fk_patient_client FOREIGN KEY ( client_number ) REFERENCES group_7.client( id );
-
-ALTER TABLE group_7.prescribes ADD CONSTRAINT fk_prescribes_products FOREIGN KEY ( product_id ) REFERENCES group_7.products( id );
-
-ALTER TABLE group_7.prescribes ADD CONSTRAINT fk_appointment FOREIGN KEY ( appointment_id ) REFERENCES group_7.appointment( id );
-
-ALTER TABLE group_7.room ADD CONSTRAINT fk_practice FOREIGN KEY ( practice_name ) REFERENCES group_7.practice( id );
-
-ALTER TABLE group_7.specialisation ADD CONSTRAINT fk_specialisation_worker FOREIGN KEY ( employee_id ) REFERENCES group_7.worker( id );
-
-ALTER TABLE group_7.vet_has_appointment ADD CONSTRAINT fk_worker FOREIGN KEY ( employee_id ) REFERENCES group_7.worker( id );
-
-ALTER TABLE group_7.vet_has_appointment ADD CONSTRAINT fk_appointment FOREIGN KEY ( appointment_id ) REFERENCES group_7.appointment( id );
-
-ALTER TABLE group_7.worker ADD CONSTRAINT fk_practice FOREIGN KEY ( practice_name ) REFERENCES group_7.practice( id );
-
-ALTER TABLE group_7.worker_has_time_slot ADD CONSTRAINT fk_worker FOREIGN KEY ( worker_id ) REFERENCES group_7.worker( id );
-
-ALTER TABLE group_7.worker_has_time_slot ADD CONSTRAINT fk_time_slot FOREIGN KEY ( time_slot_id ) REFERENCES group_7.time_slot( id );
+CREATE INDEX idx_animal_carer_has_appointment ON group_7.animal_carer_has_appointment  ( appointment_id, worker_id );
 
 GRANT ALL ON SCHEMA group_7 TO r1079475, r0965625, r1086252, r0998097, local_r1079475, local_r0965625, local_r1086252, local_r0998097;
 
